4e4fe8573c59cf5aa751012adf3214d1ae8a8697
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-26 17:17:54 +0530

    feat: add request interceptor to include authorization token in API requests

--- api/axios.js
+++ api/axios.js
@@ -2,5 +2,24 @@ import axios from "axios";
 
 
 export const API=axios.create({
-    baseURL:process.env.NEXT_PUBLIC_API_URL
+    baseURL:process.env.NEXT_PUBLIC_API_URL,
+    withCredentials:true,
+})
+
+
+//request interceptor
+
+API.interceptors.request.use(
+    (config)=>{
+const token =localStorage.getItem("token");
+console.log("Token from localStorage:", token); // Debugging line
+
+if(token){
+    config.headers.Authorization =`Bearer ${token}`;
+}
+return config;
+},
+
+(error)=>{
+    return Promise.reject(error)
 })
