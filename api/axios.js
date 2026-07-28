5fcfc229c90cb0990317b771eed2a849db9c8b7c
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-28 18:21:16 +0530

    feat: add response interceptor for error handling with toast notifications

--- api/axios.js
+++ api/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "sonner";
 
 
 export const API=axios.create({
@@ -23,3 +24,46 @@ return config;
 (error)=>{
     return Promise.reject(error)
 })
+
+// API.interceptors.response.use(
+//   (response) => response,
+
+//   (error) => {
+//     if (error.response) {
+//       switch (error.response.status) {
+//         case 400:
+//           toast.error(error.response.data.message);
+//           break;
+
+//         case 401:
+//           toast.error("Your session has expired. Please log in again.");
+//           localStorage.removeItem("token");
+//           window.location.href = "/login";
+//           break;
+
+//         case 403:
+//           toast.error("Access denied.");
+//           break;
+
+//         case 404:
+//           toast.error(error.response.data.message);
+//           break;
+
+//         case 409:
+//           toast.error(error.response.data.message);
+//           break;
+
+//         case 500:
+//           toast.error("Something went wrong. Please try again later.");
+//           break;
+
+//         default:
+//           toast.error(error.response.data.message || "An unexpected error occurred.");
+//       }
+//     } else {
+//       toast.error("Unable to connect to the server.");
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
